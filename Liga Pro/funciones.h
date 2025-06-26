550fa913b2496a5b89d3099f1dabdaedbadbd15c
Tomás <tomas.gonzalez@udla.edu.ec>
2025-06-26 07:02:38 -0500

    Update funciones.h

--- Liga Pro/funciones.h
+++ Liga Pro/funciones.h
@@ -34,6 +34,15 @@ typedef struct
     char jugadores[500];
 } EquipoJugadores;
 
+// Estructura para jugadores por equipo
+typedef struct
+{
+    char nombre[50];
+    char equipo[50];
+    int goles;
+    int indice;
+} JugadorEquipo;
+
 // Declaraciones de funciones para guardar datos
 void guardarJugadores(Jugador jugadores[], int cantidad);
 void guardarPartidos(Partido partidos[], int cantidad);
@@ -52,3 +61,13 @@ void inicializarPartidos(Partido partidos[]);
 void inicializarTabla(EquipoTabla equipos[]);
 void inicializarJugadoresClave(EquipoJugadores equipos[]);
 void inicializarDatos();
+
+// Nuevas funciones para entrada interactiva de datos
+void inicializarJugadoresEquipos();
+void ingresarResultadoPartido();
+void mostrarJugadoresEquipo(char *nombreEquipo);
+void actualizarTablaEquipos(char *equipoGanador, char *equipoPerdedor, int golesGanador, int golesPerdedor);
+void actualizarTablaEmpate(char *equipo1, char *equipo2, int goles1, int goles2);
+void cargarJugadoresEquipo(JugadorEquipo jugadores[], char *equipo, int *cantidad);
+void guardarJugadoresEquipo();
+void calcularPosicionesTabla(EquipoTabla equipos[], int cantidad);
